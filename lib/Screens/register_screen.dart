3b0cca695014d525d9b3d68774801d43999bcd33
Dafi <satria1002@gmail.com>
2022-06-09 00:48:40 +0700

    fix overflow at register screen

--- lib/Screens/register_screen.dart
+++ lib/Screens/register_screen.dart
@@ -14,69 +14,73 @@ class _RegisterScreenState extends State<RegisterScreen> {
       debugShowCheckedModeBanner: false,
       title: 'register',
       home: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.fromLTRB(117, 76, 117, 63),
-              child: Text('Sign Up',
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(30, 0, 15, 45),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Name'),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.person),
-                        labelText: 'Enter your name'),
-                  )
-                ],
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(117, 76, 117, 63),
+                child: Text('Sign Up',
+                    style:
+                        TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(30, 0, 15, 45),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Email Address'),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.mail),
-                        labelText: 'Enter your email address'),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(30, 0, 15, 45),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Password'),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.lock),
-                        labelText: 'Enter your password'),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(30, 0, 15, 115),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size.fromHeight(50),
+              Container(
+                margin: EdgeInsets.fromLTRB(30, 0, 15, 45),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Name'),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.person),
+                          labelText: 'Enter your name'),
+                    )
+                  ],
                 ),
-                onPressed: () {},
-                child: Text('CREATE ACCOUNT'),
               ),
-            )
-          ],
+              Container(
+                margin: EdgeInsets.fromLTRB(30, 0, 15, 45),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Email Address'),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.mail),
+                          labelText: 'Enter your email address'),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(30, 0, 15, 45),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Password'),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.lock),
+                          labelText: 'Enter your password'),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(30, 0, 15, 115),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(50),
+                  ),
+                  onPressed: () {},
+                  child: Text('CREATE ACCOUNT'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
