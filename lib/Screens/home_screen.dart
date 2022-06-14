aba2ee95b0de6bc8a174858cbb9bc1b9388b292e
user <brigas29@gmail.com>
2022-06-14 07:26:32 +0700

    HomePage_progress

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,156 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+
+import '../ProjectsPage/ProgressCard.dart';
+
+class TasksPage extends StatefulWidget {
+  const TasksPage({
+    Key? key,
+    required this.Goback,
+  }) : super(key: key);
+  final void Function(int) Goback;
+  @override
+  State<TasksPage> createState() => _TasksPageState();
+}
+
+class _TasksPageState extends State<TasksPage> {
+  DateTime _selectedDate = DateTime.now();
+  void _onDateChange(DateTime date) {
+    this.setState(() {
+      this._selectedDate = date;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        color: Color.fromRGBO(242, 244, 255, 1),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(25),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  )),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          widget.Goback(1);
+                        },
+                        child: Icon(
+                          Icons.arrow_back_rounded,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                      ),
+                      Icon(
+                        Icons.search_rounded,
+                        color: Colors.black,
+                        size: 30,
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '${DateFormat('MMM, d').format(this._selectedDate)}',
+                        style: GoogleFonts.montserrat(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const AddNewTask()));
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 123, 0, 245),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                              Text(
+                                "Add task",
+                                style: GoogleFonts.montserrat(
+                                  color: Colors.white,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 25),
+                  DatePicker(
+                    DateTime.now(),
+                    initialSelectedDate: this._selectedDate,
+                    selectionColor: Color.fromARGB(255, 123, 0, 245),
+                    onDateChange: this._onDateChange,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Task",
+                    style: GoogleFonts.montserrat(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(children: [
+                      ProgressCard(
+                          ProjectName: "Project", CompletedPercent: 30),
+                      ProgressCard(
+                          ProjectName: "Project", CompletedPercent: 30),
+                      ProgressCard(
+                          ProjectName: "Project", CompletedPercent: 30),
+                      ProgressCard(
+                          ProjectName: "Project", CompletedPercent: 30),
+                    ]),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
