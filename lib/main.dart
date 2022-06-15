b7372ae297f11843fb868e49fbe459308c760964
dafisatria <satria1002@gmail.com>
2022-06-16 02:08:51 +0700

    add user screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_coding_calendar/Screens/register_screen.dart';
+import 'package:flutter_coding_calendar/Screens/user_screen.dart';
 
 import 'Screens/login_screen.dart';
 
@@ -17,8 +18,16 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+          brightness: Brightness.light,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: "Muli",
       ),
-      home: RegisterScreen(),
+      home: UserScreen(),
     );
   }
 }
